baeb333ab0f13644e6c51214d37562e0e5ba2575
Crownless-0x0 <esakkiappanesanth@gmail.com>
2026-07-10 23:45:42 +0530

    Added datas and bug fixes

--- data_analysis.py
+++ data_analysis.py
@@ -5,8 +5,30 @@ import mysql.connector
 conn = lr.get_connected()
 cursor = conn.cursor()
 
-cursor.execute("""SELECT close FROM stocks""")
+def import_close_values():
+    cursor.execute("""SELECT `Close` FROM stocks ORDER BY `Date`""")
+    prices = [row[0] for row in cursor.fetchall()]
+    return prices
 
-prices = [row[0] for row in cursor.fetchall()]
+def calculate_daily_return():
+    prices = import_close_values()
+    d_return = []
+    for i in range(1, len(prices)):
+        d_return.append((prices[i] - prices[i-1]) / prices[i-1])
+    return d_return
 
-print(prices)
+def mean_return():
+    d_return = calculate_daily_return()
+    return sum(d_return) / len(d_return)
+
+def volatility():
+    daily_return = calculate_daily_return()
+    meu = mean_return()   # a single scalar mean, not a list
+    n = len(daily_return)
+
+    sum_return_diff_meu = 0
+    for r in daily_return:
+        sum_return_diff_meu += (r - meu) ** 2
+
+    var = sum_return_diff_meu / (n - 1)   # sample variance
+    return math.sqrt(var)                  # volatility = standard deviation
